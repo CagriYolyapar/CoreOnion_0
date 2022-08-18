9d003d8a78ee5733942f5691a86089070daec0db
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-08-18 11:07:22 +0300

    DAL katmanı tamamlandı

---
+++ Project.DAL/Repositories/Concretes/AppUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class AppUserRepository : BaseRepository<AppUser>, IAppUserRepository
+    {
+        //Sizin kendinize özel Crud işlemlerinin yine olması gerekir. ANcak unutmayın ki Identity yapısının özel şifrelemeler ve yetkilendirmeleri icin hazır async metotları vardır... Bu metotların kullanımı icin de ekstra olarak bu Repository'de ayrı alanlar acmak en dogrusudur... Bu metotlar Manager sınıfları icerisinde bulundurulur(UserManager,SignInManager Identity'de gömülü olan sınıflardır...) Bu sınıflar Dependency Injection ile calısırlar... Ve dolayısıyla Constructor based injection yapılabilir...
+
+        UserManager<AppUser> _userManager;
+        SignInManager<AppUser> _signInManager;
+
+        public AppUserRepository(MyContext db,UserManager<AppUser> userManager,SignInManager<AppUser> signInManager):base(db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        //Ozel bir Identity metodu async seklinde tanımlanmalıdır...Cünkü siz burada sonucta bir API kullanıyorsunuz...Ve bu API requestlerinin bloklanmadan devam edebilmesi icin ,işlemleri tamamlayarak hatasız bir şekilde akıcı olması icin await keyword'unun kullanılması gerekir...Calısma yaptıgınız metot icerisinde await keyword'unun kullanılabilmesi icin o metodun async seklinde tanımlanması gerekir ve Task döndürmesi gerekir
+
+        public async Task<bool> AddUser(AppUser item)
+        {
+            //Sadece Asenkron olarak yaratılmkıs (asnyc marklı) metotlar icerisinde await kullanabilirsiniz...
+
+            IdentityResult result =await _userManager.CreateAsync(item, item.PasswordHash);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(item, isPersistent: false); //isPersistent durumu Cookie'de dursun mu durmasın mı
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Project.DAL/Repositories/Concretes/BaseRepository.cs
@@ -0,0 +1,144 @@
+﻿using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    //Artık bu mimaride BaseRepository Abstract olmayacak
+    public class BaseRepository<T> : IRepository<T> where T : class, IEntity
+    {
+       
+       protected MyContext _db;
+
+        public BaseRepository(MyContext db)
+        {
+            _db = db;
+        }
+
+        protected void Save()
+        {
+            _db.SaveChanges();
+        }
+        public void Add(T item)
+        {
+            _db.Set<T>().Add(item);
+            Save();
+        }
+
+        public void AddRange(List<T> list)
+        {
+            _db.Set<T>().AddRange(list);
+            Save();
+        }
+
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().Any(exp);
+        }
+
+        public void Delete(T item)
+        {
+            item.Status = ENTITIES.Enums.DataStatus.Deleted;
+            item.DeletedDate = DateTime.Now;
+            Save();
+        }
+
+        public void DeleteRange(List<T> list)
+        {
+            foreach (T item in list)
+            {
+                Delete(item);
+            }
+        }
+
+        public void Destroy(T item)
+        {
+            _db.Set<T>().Remove(item);
+            Save();
+        }
+
+        public void DestroyRange(List<T> list)
+        {
+            _db.Set<T>().RemoveRange(list);
+            Save();
+        }
+
+        public T Find(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().FirstOrDefault(exp);
+        }
+
+        public List<T> GetActives()
+        {
+            return Where(x => x.Status != ENTITIES.Enums.DataStatus.Deleted);
+        }
+
+        public List<T> GetAll()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public List<T> GetFirstDatas(int number)
+        {
+            return _db.Set<T>().OrderBy(x => x.CreatedDate).Take(number).ToList();
+        }
+
+        public List<T> GetLastDatas(int number)
+        {
+            return _db.Set<T>().OrderByDescending(x => x.CreatedDate).Take(number).ToList();
+        }
+
+        public List<T> GetModifieds()
+        {
+            return Where(x => x.Status == ENTITIES.Enums.DataStatus.Updated);
+        }
+
+        public List<T> GetPassives()
+        {
+            return Where(x => x.Status == ENTITIES.Enums.DataStatus.Deleted);
+        }
+
+        public object Select(Expression<Func<T, object>> exp)
+        {
+            return _db.Set<T>().Select(exp).ToList();
+        }
+
+        public IQueryable<X> SelectViaClass<X>(Expression<Func<T, X>> exp)
+        {
+            return _db.Set<T>().Select(exp);
+        }
+
+        public void Update(T item)
+        {
+            item.Status = ENTITIES.Enums.DataStatus.Updated;
+            item.ModifiedDate = DateTime.Now;
+            T toBeUpdated = Find(item.ID);
+            _db.Entry(toBeUpdated).CurrentValues.SetValues(item);
+            Save();
+        }
+
+        public void UpdateRange(List<T> list)
+        {
+            foreach (T item in list)
+            {
+                Update(item);
+            }
+        }
+
+        public List<T> Where(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().Where(exp).ToList();
+        }
+    }
+}
